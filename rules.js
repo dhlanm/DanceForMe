156d882cedb0da11cab3a17db4d1d04d8db8fb7e
Zach Wade <zacharywade@gmail.com>
2014-12-02 13:21:01 -0500

    zach's dumb semantics

--- rules.js
+++ rules.js
@@ -15,22 +15,22 @@
 //if two players are jumping on one another and the top player stops before the bottom player, the top player will hang in the air. 
 
 var myState;
-jumpStr=10;		   //default to 10
-maxProj=3;		   //default to 3
-playerSpeed=5;     //less than 20 for no bugs. default to 5
-deathfloattime=40; //time the player can hang in the air at their starting spot before falling
-maxhp=5;
-maxlives=5;
+var jumpStr=10;		   //default to 10
+var maxProj=3;		   //default to 3
+var playerSpeed=5;	 //less than 20 for no bugs. default to 5
+var deathfloattime=40; //time the player can hang in the air at their starting spot before falling
+var maxhp=5;
+var maxlives=5;
 
 shapeProtoGen = function(type) {
 	p = {
 		draw: function(ctx) {
-		  ctx.fillStyle = this.fill;
-		  ctx.fillRect(this.x, this.y, this.w, this.h);
+			ctx.fillStyle = this.fill;
+			ctx.fillRect(this.x, this.y, this.w, this.h);
 		}, 
 		collides: function(shape, debug) {
-			if(xcollision(this, shape) && ycollision(this, shape)) {
-				if(softxcollision(new Shape(this.lastx, this.lasty, this.w, this.h), shape) || this.lastx==shape.x || this.lastx+this.w==shape.x+shape.w) {
+			if(XCollision(this, shape) && YCollision(this, shape)) {
+				if(softXCollision(new Shape(this.lastx, this.lasty, this.w, this.h), shape) || this.lastx==shape.x || this.lastx+this.w==shape.x+shape.w) {
 					if(this.y<shape.y)
 						return 'top';
 					return 'bottom';
@@ -92,74 +92,74 @@ shapeProtoGen = function(type) {
 }
 
 Shape = function(x, y, w, h, fill, mobile, speed) {
-  this.x = x || 0;
-  this.y = y || 0;
-  this.w = w || 20;
-  this.h = h || 20;
-  this.fill = fill || '#AAAAAA';
-  this.speed = speed || 5; 
-  this.mobile = mobile || false; 
-  this.xto = this.x;
+	this.x = x || 0;
+	this.y = y || 0;
+	this.w = w || 20;
+	this.h = h || 20;
+	this.fill = fill || '#AAAAAA';
+	this.speed = speed || 5; 
+	this.mobile = mobile || false; 
+	this.xto = this.x;
 }
 Shape.prototype = shapeProtoGen("shape")
 
 
 Projectile = function(x, y, w, h, fill, xv, player){
-  this.x = x || 0;
-  this.y = y || 0;
-  this.w = w || 1;
-  this.h = h || 1;
-  this.fill = fill || '#AAAAAA';
-  this.xv = xv || 1;
-  this.player = player || 0;
+	this.x = x || 0;
+	this.y = y || 0;
+	this.w = w || 1;
+	this.h = h || 1;
+	this.fill = fill || '#AAAAAA';
+	this.xv = xv || 1;
+	this.player = player || 0;
 }
 Projectile.prototype = shapeProtoGen("projectile"); 
 
 Player = function(x, y, w, h, fill, speed, player) {
-  Shape.apply(this,arguments)
-  this.x = x || 0;
-  this.y = y || 0;
-  this.w = w || 20;
-  this.h = h || 20;
-  this.fill = fill || '#AAAAAA';
-  this.speed = speed || 5; 
-  this.player = player || 0;
-  this.xto = this.x;
-  this.yv = 0;
-  this.ya = .05*this.h;
-  this.lastx = x;
-  this.lasty = y;
-  this.startx = x;
-  this.starty = y;
-  this.projectiles = 0;
-  this.hp = maxhp;
-  this.lives = maxlives;
-  this.left = false;
-  this.right = false;
-  this.jumping = false;
-  this.mobile = true;
+	Shape.apply(this,arguments)
+	this.x = x || 0;
+	this.y = y || 0;
+	this.w = w || 20;
+	this.h = h || 20;
+	this.fill = fill || '#AAAAAA';
+	this.speed = speed || 5; 
+	this.player = player || 0;
+	this.xto = this.x;
+	this.yv = 0;
+	this.ya = .05*this.h;
+	this.lastx = x;
+	this.lasty = y;
+	this.startx = x;
+	this.starty = y;
+	this.projectiles = 0;
+	this.hp = maxhp;
+	this.lives = maxlives;
+	this.left = false;
+	this.right = false;
+	this.jumping = false;
+	this.mobile = true;
 }
 Player.prototype = shapeProtoGen("player"); 
 
 function between(x, min, max) {
-  return x > min && x < max;
+	return x > min && x < max;
 }
 function exbetween(x, min, max) {
-  return x >= min && x <= max;
+	return x >= min && x <= max;
 }
 
 
-softxcollision = function(shape0, shape1) {
+softXCollision = function(shape0, shape1) {
 	if((between(shape0.x, shape1.x, shape1.x+shape1.w) || between(shape1.x, shape0.x, shape0.x+shape0.w) || shape0.x==shape1.x || shape0.x+shape0.w==shape1.x+shape1.w))
 		return true;
 	return false
 }
-xcollision = function(shape0, shape1) {
+XCollision = function(shape0, shape1) {
 	if((exbetween(shape0.x, shape1.x, shape1.x+shape1.w) || exbetween(shape1.x, shape0.x, shape0.x+shape0.w)))// || (exbetween(shape0.x, shape1.x, shape1.x+shape1.w) && exbetween(shape1.x, shape0.x, shape0.x+shape0.w))) 
 		return true;
 	return false
 }
-ycollision = function(shape0, shape1) {
+YCollision = function(shape0, shape1) {
 	if(exbetween(shape0.y, shape1.y, shape1.y+shape1.h) || exbetween(shape1.y, shape0.y, shape0.y+shape0.h))
 		return true;
 	return false;
@@ -168,79 +168,79 @@ ycollision = function(shape0, shape1) {
 
 function CanvasState(canvas) {
   
-  this.canvas = canvas;
-  this.width = canvas.width;
-  this.height = canvas.height;
-  this.ctx = canvas.getContext('2d');
-  // This complicates things a little but but fixes mouse co-ordinate problems
-  // when there's a border or padding. See getMouse for more detail
-  var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
-  if (document.defaultView && document.defaultView.getComputedStyle) {
-    this.stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
-    this.stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
-    this.styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
-    this.styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
-  }
-  // Some pages have fixed-position bars (like the stumbleupon bar) at the top or left of the page
-  // They will mess up mouse coordinates and this fixes that
-  var html = document.body.parentNode;
-  this.htmlTop = html.offsetTop;
-  this.htmlLeft = html.offsetLeft;
+	this.canvas = canvas;
+	this.width = canvas.width;
+	this.height = canvas.height;
+	this.ctx = canvas.getContext('2d');
+	// This complicates things a little but but fixes mouse co-ordinate problems
+	// when there's a border or padding. See getMouse for more detail
+	var stylePaddingLeft, stylePaddingTop, styleBorderLeft, styleBorderTop;
+	if (document.defaultView && document.defaultView.getComputedStyle) {
+		this.stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingLeft'], 10)      || 0;
+		this.stylePaddingTop  = parseInt(document.defaultView.getComputedStyle(canvas, null)['paddingTop'], 10)       || 0;
+		this.styleBorderLeft  = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderLeftWidth'], 10)  || 0;
+		this.styleBorderTop   = parseInt(document.defaultView.getComputedStyle(canvas, null)['borderTopWidth'], 10)   || 0;
+	}
+	// Some pages have fixed-position bars (like the stumbleupon bar) at the top or left of the page
+	// They will mess up mouse coordinates and this fixes that
+	var html = document.body.parentNode;
+	this.htmlTop = html.offsetTop;
+	this.htmlLeft = html.offsetLeft;
 
-  this.collidables = [];  // the collections of things to be drawn
-  this.projectiles = [];
-  this.noncollidables = [];
-  this.players = [];
-  
-  myState = this;
-  var sX;
-  var sY;
-  //fixes a problem where double clicking causes text to get selected on the canvas
-  canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
-  this.addPlayer(new Player(10,100,20,20,'rgba(255,0,0,.6)', playerSpeed, 0));
-  this.addPlayer(new Player(500,100,20,20,'rgba(0,0,255,.6)', playerSpeed, 1));
-  this.blitHp(this.players[0]);
-  this.blitHp(this.players[1]);
-  this.addCollidable(new Shape(300,550,80,20,'rgba(0,0,0,.6)'));
-  
-  this.interval = 30;
-  setInterval(function() { myState.draw(); }, myState.interval);
+	this.collidables = [];  // the collections of things to be drawn
+	this.projectiles = [];
+	this.noncollidables = [];
+	this.players = [];
+	
+	myState = this;
+	var sX;
+	var sY;
+	//fixes a problem where double clicking causes text to get selected on the canvas
+	canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
+	this.addPlayer(new Player(10,100,20,20,'rgba(255,0,0,.6)', playerSpeed, 0));
+	this.addPlayer(new Player(500,100,20,20,'rgba(0,0,255,.6)', playerSpeed, 1));
+	this.blitHp(this.players[0]);
+	this.blitHp(this.players[1]);
+	this.addCollidable(new Shape(300,550,80,20,'rgba(0,0,0,.6)'));
+	
+	this.interval = 30;
+	setInterval(function() { myState.draw(); }, myState.interval);
 }
 
 CanvasState.prototype.addCollidable = function(shape) {
-  this.collidables.push(shape);
+	this.collidables.push(shape);
 }
 CanvasState.prototype.addProjectile = function(shape) {
-  this.projectiles.push(shape);
+	this.projectiles.push(shape);
 }
 CanvasState.prototype.addPlayer = function(shape) {
-  this.players.push(shape);
+	this.players.push(shape);
 }
 CanvasState.prototype.addNonCollidable = function(shape) {
-  this.noncollidables.push(shape);
+	this.noncollidables.push(shape);
 }
 
 CanvasState.prototype.blitHp = function(player) {
-  for(var i=0; i<player.hp; i++) {
-	this.addNonCollidable(new Shape(10+915*player.player+15*i, 40, 10, 10, 'rgba('+String(255*(1-player.player))+',0,'+String(255*player.player)+', .4)'));
-  }
-  for(var i=0; i<player.lives; i++) {
-	this.addNonCollidable(new Shape(10+915*player.player+15*i, 55, 10, 10, 'rgba('+String(255*(1-player.player))+',100,'+String(255*player.player)+', .4)'));
-  }
+	for(var i=0; i<player.hp; i++) {
+		this.addNonCollidable(new Shape(10+915*player.player+15*i, 40, 10, 10, 'rgba('+String(255*(1-player.player))+',0,'+String(255*player.player)+', .4)'));
+	}
+	for(var i=0; i<player.lives; i++) {
+		this.addNonCollidable(new Shape(10+915*player.player+15*i, 55, 10, 10, 'rgba('+String(255*(1-player.player))+',100,'+String(255*player.player)+', .4)'));
+	}
 }
 
 CanvasState.prototype.clear = function() {
-  this.ctx.clearRect(0, 0, this.width, this.height);
+	this.ctx.clearRect(0, 0, this.width, this.height);
 }
 
 
 CanvasState.prototype.draw = function() {
-    var ctx = this.ctx;
-    var collidables = this.collidables;
+	var ctx = this.ctx;
+	var collidables = this.collidables;
 	var noncollidables = this.noncollidables;
 	var projectiles = this.projectiles
 	var players = this.players;
-    this.clear();
+	this.clear();
 	for(var i=0; i<players.length; i++) {
 		if(players[i].left)
 			players[i].xto-=20;
@@ -252,7 +252,7 @@ CanvasState.prototype.draw = function() {
 		}
 	}
 	for (var i = 0; i < players.length+collidables.length; i++) {
-	    incoll=false;
+		incoll=false;
 		if(i>=players.length)
 			shape = collidables[i-players.length];
 		else
@@ -387,11 +387,13 @@ CanvasState.prototype.draw = function() {
 			players[i].mobile=true;
 			players[i].ya=.05*players[i].h;
 			players[i].floatticks=-1;
+		} else if(players[i].floatticks<deathfloattime*.75) {
+			players[i].mobile=true;
+		} else if(players[i].floatticks>0) {
+			players[i].floatticks--;
 		}
-		else if(players[i].floatticks<deathfloattime*.75) players[i].mobile=true;
-		else if(players[i].floatticks>0) players[i].floatticks--;
 	}
-    drawobjects(collidables, collidables.length)
+	drawobjects(collidables, collidables.length)
 	drawobjects(noncollidables, noncollidables.length)
 	drawobjects(projectiles, projectiles.length)
 	drawobjects(players, players.length)
@@ -399,10 +401,10 @@ CanvasState.prototype.draw = function() {
 }
 drawobjects = function (array, length) {
 	for (var i = 0; i < length; i++) {
-      var shape = array[i];
-      // We can skip the drawing of elements that have moved off the screen:
-      if (shape.x > myState.width || shape.y > myState.height ||
-          shape.x + shape.w < 0 || shape.y + shape.h < 0) {
+		var shape = array[i];
+		// We can skip the drawing of elements that have moved off the screen:
+		if (shape.x > myState.width || shape.y > myState.height ||
+			shape.x + shape.w < 0 || shape.y + shape.h < 0) {
 			x=array[i]
 			if(x.player!=null)
 				myState.players[x.player].projectiles--;
@@ -410,33 +412,33 @@ drawobjects = function (array, length) {
 			console.log("Out of bounds shape removed");
 			continue;
 		}
-      array[i].draw(myState.ctx);
-    }
+		array[i].draw(myState.ctx);
+	}
 }
 
 // Creates an object with x and y defined, set to the mouse position relative to the state's canvas
 // If you wanna be super-correct this can be tricky, we have to worry about padding and borders
 CanvasState.prototype.getMouse = function(e) {
-  var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
-  
-  // Compute the total offset
-  if (element.offsetParent !== undefined) {
-    do {
-      offsetX += element.offsetLeft;
-      offsetY += element.offsetTop;
-    } while ((element = element.offsetParent));
-  }
+	var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
+	
+	// Compute the total offset
+	if (element.offsetParent !== undefined) {
+		do {
+			offsetX += element.offsetLeft;
+			offsetY += element.offsetTop;
+		} while ((element = element.offsetParent));
+	}
 
-  // Add padding and border style widths to offset
-  // Also add the <html> offsets in case there's a position:fixed bar
-  offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
-  offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
+	// Add padding and border style widths to offset
+	// Also add the <html> offsets in case there's a position:fixed bar
+	offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
+	offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
 
-  mx = e.pageX - offsetX;
-  my = e.pageY - offsetY;
-  
-  // We return a simple javascript object (a hash) with x and y defined
-  return {x: mx, y: my};
+	mx = e.pageX - offsetX;
+	my = e.pageY - offsetY;
+	
+	// We return a simple javascript object (a hash) with x and y defined
+	return {x: mx, y: my};
 }
 
 // If you dont want to use <body onLoad='init()'>
@@ -477,7 +479,7 @@ function keyup(e) {
 		player0.unjump();
 	if(e.keyCode==65) //a
 		player0.leftkeyup();
-    if(e.keyCode==68) //d
+	if(e.keyCode==68) //d
 		player0.rightkeyup();
 	if(e.keyCode==37) //left
 		player1.leftkeyup();
@@ -494,33 +496,28 @@ function getRotationPoint(x, y, x0, y0, phi) {
 }
 
 function sleep(milliseconds) { //http://stackoverflow.com/questions/16873323/javascript-sleep-wait-before-continuing
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds){
+			break;
+		}
+	}
 }
 
 function zeros(dimensions) { //http://stackoverflow.com/questions/3689903/how-to-create-a-2d-array-of-zeroes-in-javascript
-    var array = [];
+	var array = [];
 
-    for (var i = 0; i < dimensions[0]; ++i) {
-        array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
-    }
+	for (var i = 0; i < dimensions[0]; ++i) {
+		array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+	}
 
-    return array;
+	return array;
 }
 
 function init() {
-  var s = new CanvasState(document.getElementById('canvas1'));
-  document.onkeydown=keypress;
-  document.onkeyup=keyup;
-  //s.addShape(new Shape(40,40,50,50)); // The default is gray
-  //s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
-  // Lets make some partially transparent
-  //s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
-  //s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
+	var s = new CanvasState(document.getElementById('canvas1'));
+	document.onkeydown=keypress;
+	document.onkeyup=keyup;
 }
 
 // Now go make something amazing!
